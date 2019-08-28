8d5132da132bc033fd30385fc427300a29dd5a33
lalyns <blackalsl@gmail.com>
2019-08-28 21:48:09 +0900

    8/28 아이템 기초 구현

---
+++ 빌드/Source/Assets/Scripts/Object/Item/Gauge.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gauge : MonoBehaviour
+{
+    public float _GaugeValue;
+
+    public void GaugePlayer()
+    {
+        float gauge = 10;
+
+        //조건 판단 (현재게이지 + 추가게이지량 > 최대게이지량)일때, 어떻게 처리할것인지 조건 추가
+        gauge += _GaugeValue;
+    }
+
+}
---
+++ 빌드/Source/Assets/Scripts/Object/Item/Healing.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healing : MonoBehaviour
+{
+    [Tooltip("True: HOT, False: NORMAL")]
+    public bool HOT;
+
+    public int _HealValue;
+
+    [Header("HOT가 True일떄")]
+    public int _HealingTime;
+    public float interval = 0.2f;
+
+    public void HealPlayer()
+    {
+        // 플레이어 체력 관련 정보 삽입
+        int hp = 100;
+
+        // 플레이어 체력 회복
+
+        if (!HOT)
+        {
+            //조건 판단 (현재채력 + 힐량 > 최대체력)일때, 어떻게 처리할것인지 조건 추가
+
+            hp += _HealValue;
+        }
+
+        else
+        {
+            for(int i=0; i<_HealingTime; i++)
+            {
+                //조건 판단 (현재채력 + 힐량 > 최대체력)일때, 어떻게 처리할것인지 조건 추가
+
+                hp += _HealValue;
+
+                StartCoroutine("TimeWaiting");
+            }
+        }
+
+        // 플레이어 체력 적용
+    }
+
+    // 회복 지연시간
+    public IEnumerator TimeWaiting()
+    {
+        yield return new WaitForSeconds(interval);
+    }
+}
