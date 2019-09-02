491f655eb37f308b9956b4192f8567097c517108
lalyns <blackalsl@gmail.com>
2019-09-02 18:27:15 +0900

    맥 스킬 상태 및 더미이펙트 추가

--- 빌드/Source/Assets/Scripts/Character/Mac/MacState/MacSKILL.cs
+++ 빌드/Source/Assets/Scripts/Character/Mac/MacState/MacSKILL.cs
@@ -2,17 +2,85 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MacSKILL : MonoBehaviour
+public class MacSKILL : MacFSMState
 {
-    // Start is called before the first frame update
-    void Start()
+    public float _AttackBeforeTime = 0.8f;
+    public float _AttackTime = 3.0f;
+    public float _AfterAttackTime = 1.0f;
+    public float _Time = 0.0f;
+
+    bool _CreateBall = false;
+    bool _SetBall = false;
+    public Transform bullet;
+
+    public override void BeginState()
     {
-        
+        base.BeginState();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void EndState()
     {
-        
+        base.EndState();
+    }
+
+    private void Update()
+    {
+        _Time += Time.deltaTime;
+
+        if (GameLib.DistanceToCharacter(_manager.CC, _manager.PlayerCapsule) < _manager.Stat.statData._AttackRange)
+        {
+            if (_Time > _AttackTime)
+            {
+                _manager._MR.material = _manager.Stat._BeforeAttackMat;
+
+
+                if (!_CreateBall)
+                {
+                    transform.LookAt(_manager.PlayerCapsule.transform);
+                    bullet = Instantiate(_manager.Stat._SkillEffect,
+                    _manager._AttackTransform.position,
+                    Quaternion.identity).transform;
+                    bullet.transform.parent = this.transform;
+                    _CreateBall = true;
+                }
+
+
+                if (_Time > _AttackTime + _AttackBeforeTime)
+                {
+                    _manager._MR.material = _manager.Stat._AttackMat;
+
+
+                    if (!_SetBall)
+                    {
+                        bullet.GetComponent<Bullet>().LookAtTarget(_manager.PlayerCapsule.transform);
+                        bullet.GetComponent<Bullet>().dir = GameLib.DirectionToCharacter(_manager.CC, _manager.PlayerCapsule);
+                        bullet.GetComponent<Bullet>()._Move = true;
+
+                        _SetBall = true;
+                    }
+
+                }
+
+                if (_Time > _AttackTime + _AttackBeforeTime + _AfterAttackTime)
+                {
+                    _Time = 0.0f;
+                    _CreateBall = false;
+                    _SetBall = false;
+
+                    _manager.SetState(MacState.RUNAWAY);
+                }
+            }
+
+        }
+        else
+        {
+            _manager.SetState(MacState.CHASE);
+        }
+
+    }
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
     }
 }
