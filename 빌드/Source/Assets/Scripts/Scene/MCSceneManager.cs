a6215d983c56270d98110e9888ee0174b352db0c
lalyns <blackalsl@gmail.com>
2019-09-30 00:32:55 +0900

    씬매니저 통합 준비

---
+++ 빌드/Source/Assets/Scripts/Scene/MCSceneManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MC.SceneDirector
+{
+    public class MCSceneManager : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        public void StartSceneButton()
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
