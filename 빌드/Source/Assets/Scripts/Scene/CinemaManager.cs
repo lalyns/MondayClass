4814b93648843ff400920977c1dd19d85350bc01
lalyns <blackalsl@gmail.com>
2019-09-30 23:56:41 +0900

    연출 추가

--- 빌드/Source/Assets/Scripts/Scene/CinemaManager.cs
+++ 빌드/Source/Assets/Scripts/Scene/CinemaManager.cs
@@ -50,6 +50,10 @@ namespace MC.SceneDirector
 
         public static void CinemaStart(PlayableDirector cinema)
         {
+            PlayerFSMManager.Instance.transform.position = PlayerFSMManager.Instance.Anim.transform.position;
+
+            PlayerFSMManager.Instance.transform.LookAt(MissionManager.Instance.CurrentMission.Exit.transform);
+
             cinema.gameObject.SetActive(true);
             Instance.currentDirector = cinema;
             cinema.Play();
