4045e3047907d1a46816b58411185438c34f7a16
pshyeok321 <pshyeok321@gmail.com>
2019-10-01 00:02:32 +0900

    풀받기위한 푸시

--- 빌드/Source/Assets/Scripts/Character/RedHat/RedHatFSMManager.cs
+++ 빌드/Source/Assets/Scripts/Character/RedHat/RedHatFSMManager.cs
@@ -168,7 +168,7 @@ public class RedHatFSMManager : FSMManager
             StartCoroutine(Shake.instance.ShakeCamera(0.1f, 0.3f, 0.1f));
         if (attackType == AttackType.SKILL1)
             StartCoroutine(Shake.instance.ShakeCamera(0.05f, 0.1f, 0.1f));
-        if (attackType == AttackType.SkILL2)
+        if (attackType == AttackType.SKILL2)
             StartCoroutine(Shake.instance.ShakeCamera(0.15f, 0.1f, 0.1f));
         //if (attackType == AttackType.SKILL3)
         //    StartCoroutine(Shake.instance.ShakeCamera(0.01f, 0.01f, 0.01f));
@@ -226,7 +226,7 @@ public class RedHatFSMManager : FSMManager
             case AttackType.SKILL1:
                 return 3;
 
-            case AttackType.SkILL2:
+            case AttackType.SKILL2:
                 return 4;
 
             case AttackType.SKILL3:
@@ -287,7 +287,7 @@ public class RedHatFSMManager : FSMManager
             {
                 try
                 {
-                    OnHitForMonster(AttackType.SkILL2);
+                    OnHitForMonster(AttackType.SKILL2);
                 }
                 catch
                 {
