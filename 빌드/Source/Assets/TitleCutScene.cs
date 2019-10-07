6d34015261d5ad40d2c6f1430f24db84c1086c00
lalyns <blackalsl@gmail.com>
2019-10-07 21:13:09 +0900

    컷씬 추가

---
+++ 빌드/Source/Assets/TitleCutScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MC.SceneDirector;
+using UnityEngine.UI;
+
+public class TitleCutScene : MonoBehaviour
+{
+    public Sprite[] cutSceneSprite;
+    public Image cutScene;
+
+    public int currentCutSceneNumber = 0;
+
+    public void CutScene()
+    {
+        if(currentCutSceneNumber == cutSceneSprite.Length)
+        {
+            MCSceneManager.Instance.NextScene(MCSceneManager.ANNIHILATION);
+        }
+        else
+        {
+            cutScene.sprite = cutSceneSprite[currentCutSceneNumber++];
+        }
+    }
+    
+}
