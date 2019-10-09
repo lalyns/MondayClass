8870994aae6d3bada8879c199280fc5f766fd8a7
lalyns <blackalsl@gmail.com>
2019-10-09 18:59:04 +0900

    몬스터 데미지 공식 수정

--- 빌드/Source/Assets/Scripts/Character/Tiber/TiberWeapon.cs
+++ 빌드/Source/Assets/Scripts/Character/Tiber/TiberWeapon.cs
@@ -27,7 +27,11 @@ public class TiberWeapon : MonoBehaviour
         {
             if (other.transform.tag == "Player")
             {
-                var hitTarget = GameLib.SimpleDamageProcess(this.transform, 0.01f, "Player", Tiber.Stat, 50);
+                float damage = Tiber.Stat.damageCoefiiecient[0] * 0.01f *
+                (Tiber.Stat.Str + Tiber.Stat.addStrPerRound * GameStatus.Instance.StageLevel)
+                - PlayerFSMManager.Instance.Stat.Defense;
+
+                var hitTarget = GameLib.SimpleDamageProcess(this.transform, 0.01f, "Player", Tiber.Stat, damage);
                 Invoke("AttackSupport", 0.5f);
                 _Dameged = true;
                 PlayerFSMManager.Instance.SetState(PlayerState.HIT2);
@@ -37,7 +41,11 @@ public class TiberWeapon : MonoBehaviour
         {
             if (other.transform.tag == "Player")
             {
-                var hitTarget = GameLib.SimpleDamageProcess(this.transform, 0.01f, "Player", Tiber.Stat, 10);
+                float damage = Tiber.Stat.damageCoefiiecient[1] * 0.01f *
+                (Tiber.Stat.Str + Tiber.Stat.addStrPerRound * GameStatus.Instance.StageLevel)
+                - PlayerFSMManager.Instance.Stat.Defense;
+
+                var hitTarget = GameLib.SimpleDamageProcess(this.transform, 0.01f, "Player", Tiber.Stat, damage);
                 Invoke("AttackSupport", 0.5f);
                 _Dameged = true;
             }
