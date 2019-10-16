0e056e748ab6eddefa821c46d739181fab26bf90
lalyns <blackalsl@gmail.com>
2019-10-17 01:18:01 +0900

    튜토리얼 씬 수정

--- 빌드/Source/Assets/Scripts/Object/Mission/Tutorial/MissionTutorial.cs
+++ 빌드/Source/Assets/Scripts/Object/Mission/Tutorial/MissionTutorial.cs
@@ -1,36 +1,87 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MC.UI;
 
 namespace MC.Mission
 {
-    public enum TutorialState
+    public enum TutorialEvent
     {
         None = 0,
-        FirstTutorial,
+        Move,
+        Dodge,
+        Attack,
     }
 
-    public class MissionTutorial : MissionBase
+    public class MissionTutorial : MonoBehaviour
     {
-        public TutorialState currentTutorial = TutorialState.None;
+        public TutorialEvent currentTutorial = TutorialEvent.None;
+        public UITutorial tutorialUI;
+
+        bool tutorial = false;
 
         // Start is called before the first frame update
-        protected override void Awake()
+        void Awake()
         {
-            base.Awake();
+            //base.Awake();
 
-            currentTutorial = TutorialState.None;
+            currentTutorial = TutorialEvent.None;
         }
 
         // Update is called once per frame
-        protected override void Start()
+        void Start()
         {
-            base.Start();
+            //.Start();
         }
 
-        protected override void Update()
+        void Update()
         {
-            base.Update();
+            // base.Update();
+
+            if(currentTutorial == TutorialEvent.None && !tutorial)
+            {
+                GameStatus.currentGameState = CurrentGameState.Dialog;
+                var dialogEvent = GameManager.Instance.GetComponent<DialogEvent>();
+
+                tutorialUI.gameObject.SetActive(false);
+                UserInterface.DialogSetActive(true);
+                UserInterface.Instance.Dialog.SetDialog(dialogEvent.dialogs[0],
+                    () => {
+                        GameStatus.currentGameState = CurrentGameState.Tutorial;
+                        currentTutorial = TutorialEvent.Move;
+                        tutorialUI.gameObject.SetActive(true);
+                    });
+
+                tutorial = true;
+            }
+
+            if (currentTutorial == TutorialEvent.Move)
+            {
+                if (Input.GetKeyDown(KeyCode.W))
+                {
+                    tutorialUI.move.W.sprite = tutorialUI.move.WSprites[1];
+                }
+
+                if (Input.GetKeyDown(KeyCode.S))
+                {
+                    tutorialUI.move.S.sprite = tutorialUI.move.SSprites[1];
+                }
+
+                if (Input.GetKeyDown(KeyCode.A))
+                {
+                    tutorialUI.move.A.sprite = tutorialUI.move.ASprites[1];
+                }
+
+                if (Input.GetKeyDown(KeyCode.D))
+                {
+                    tutorialUI.move.D.sprite = tutorialUI.move.DSprites[1];
+                }
+            }
+        }
+
+        void NextTutorial(TutorialEvent state)
+        {
+            currentTutorial = state;
         }
     }
 }
