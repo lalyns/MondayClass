eaf75e06da717cf3bfe8a02874ced9001f7cf842
pshyeok321 <pshyeok321@gmail.com>
2019-10-19 06:33:31 +0900

    스킬1번구현

--- 빌드/Source/Assets/Scripts/Character/Player/PlayerState/PlayerIDLE2.cs
+++ 빌드/Source/Assets/Scripts/Character/Player/PlayerState/PlayerIDLE2.cs
@@ -15,6 +15,7 @@ public class PlayerIDLE2 : FSMState
         _manager.Skill1Return(_manager.Skill1_Effects, _manager.Skill1_Special_Effects, _manager.isNormal);
         _manager.Skill1Return(_manager.Skill1_Shoots, _manager.Skill1_Special_Shoots, _manager.isNormal);
         _manager.Skill1PositionSet(_manager.Skill1_Effects, _manager.Skill1_Shoots, _manager.Skill1_Special_Shoots, _manager.isNormal);
+        
     }
 
     public override void EndState()
