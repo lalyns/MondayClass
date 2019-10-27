e3dc2cfbb897b5776c2f1f524f576d89653061a3
kangilgu <kosd2209@gmail.com>
2019-10-27 23:46:36 +0900

    엔딩 씬

---
+++ 빌드/Source/Assets/Scenes/강일구/Rolling.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rolling : MonoBehaviour
+{
+
+    public int speed = 20;
+
+    void start()
+    {
+        
+
+}
+
+    void Update()
+    {
+
+    }
+}
